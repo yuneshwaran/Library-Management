26cdf2e4c029bf16639683485ea8f742fd60d49e
Yuneshwaran <126149253+yuneshwaran@users.noreply.github.com>
2024-06-01 17:49:35 +0530

    Update dbUtility.java
    removed crerdentials

--- src/main/java/org/example/dbUtility.java
+++ src/main/java/org/example/dbUtility.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class dbUtility {
     private static final String URL = "jdbc:postgresql://localhost:5433/library";
-    private static final String USER = "yunesh";
-    private static final String PASSWORD = "yuk1";
+    private static final String USER = " ";
+    private static final String PASSWORD = " ";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
